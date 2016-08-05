f386a9cfa38dc82d50a6aaf1fdb5ad1c31c8428e
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2016-08-05 14:18:33 +0900

    Build the structure with Android Studio.

---
+++ app/src/main/java/neko_daisuki/java_conf/gr/jp/contentprovierviewer/MainActivity.java
@@ -0,0 +1,13 @@
+package neko_daisuki.java_conf.gr.jp.contentprovierviewer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
